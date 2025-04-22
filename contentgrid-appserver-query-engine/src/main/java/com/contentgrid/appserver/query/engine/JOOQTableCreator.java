b4f9c9f318bf95a432e097a4049eee663df295e9
niels cappelle <niels.cappelle@xenit.eu>
2025-04-22 09:26:45 +0200

    ACC-2018 add JOOQUtils

--- contentgrid-appserver-query-engine/src/main/java/com/contentgrid/appserver/query/engine/JOOQTableCreator.java
+++ contentgrid-appserver-query-engine/src/main/java/com/contentgrid/appserver/query/engine/JOOQTableCreator.java
@@ -46,9 +46,9 @@ public class JOOQTableCreator {
                 case ManyToManyRelation manyToManyRelation -> {
                     // Create the join table
                     dslContext.createTable(manyToManyRelation.getJoinTable().getValue())
-                            .column(resolveField(manyToManyRelation.getSourceReference(), sourceEndPoint.getEntity().getPrimaryKey()
+                            .column(JOOQUtils.resolveField(manyToManyRelation.getSourceReference(), sourceEndPoint.getEntity().getPrimaryKey()
                                     .getType(), true))
-                            .column(resolveField(manyToManyRelation.getTargetReference(), targetEndPoint.getEntity().getPrimaryKey()
+                            .column(JOOQUtils.resolveField(manyToManyRelation.getTargetReference(), targetEndPoint.getEntity().getPrimaryKey()
                                     .getType(), true))
                             .primaryKey(manyToManyRelation.getSourceReference().getValue(), manyToManyRelation.getTargetReference().getValue())
                             .constraint(DSL.foreignKey(manyToManyRelation.getSourceReference().getValue())
@@ -65,7 +65,7 @@ public class JOOQTableCreator {
 
     private void createRelation(DSLContext dslContext, RelationEndPoint owningEndPoint, RelationEndPoint otherEndPoint, ColumnName reference, boolean unique) {
         dslContext.alterTable(owningEndPoint.getEntity().getTable().getValue())
-                .add(resolveField(reference, otherEndPoint.getEntity().getPrimaryKey().getType(), owningEndPoint.isRequired()),
+                .add(JOOQUtils.resolveField(reference, otherEndPoint.getEntity().getPrimaryKey().getType(), owningEndPoint.isRequired()),
                         DSL.foreignKey(reference.getValue()).references(otherEndPoint.getEntity().getTable().getValue(),
                                 otherEndPoint.getEntity().getPrimaryKey().getColumn().getValue()))
                 .execute();
@@ -78,7 +78,7 @@ public class JOOQTableCreator {
 
     private void createTableForEntity(DSLContext dslContext, Entity entity) {
         var step = dslContext.createTable(entity.getTable().getValue())
-                .column(resolveField(entity.getPrimaryKey().getColumn(), entity.getPrimaryKey().getType(), true))
+                .column(JOOQUtils.resolveField(entity.getPrimaryKey().getColumn(), entity.getPrimaryKey().getType(), true))
                 .primaryKey(entity.getPrimaryKey().getColumn().getValue());
         for (var attribute : entity.getAttributes()) {
             step = createColumnsForAttribute(step, attribute);
@@ -89,7 +89,7 @@ public class JOOQTableCreator {
     private CreateTableElementListStep createColumnsForAttribute(CreateTableElementListStep step, Attribute attribute) {
         switch (attribute) {
             case SimpleAttribute simpleAttribute -> {
-                var result = step.column(resolveField(simpleAttribute));
+                var result = step.column(JOOQUtils.resolveField(simpleAttribute));
                 if (simpleAttribute.getConstraint(UniqueConstraint.class).isPresent()) {
                     result = result.constraint(DSL.unique(simpleAttribute.getColumn().getValue()));
                 }
@@ -103,22 +103,4 @@ public class JOOQTableCreator {
             }
         }
     }
-
-    private static Field<?> resolveField(SimpleAttribute attribute) {
-        return resolveField(attribute.getColumn(), attribute.getType(), attribute.getConstraint(RequiredConstraint.class).isPresent());
-    }
-
-    private static Field<?> resolveField(ColumnName column, SimpleAttribute.Type type, boolean required) {
-        var dataType = switch (type) {
-            case UUID -> SQLDataType.UUID;
-            case TEXT -> SQLDataType.CLOB;
-            case LONG -> SQLDataType.BIGINT;
-            case DOUBLE -> SQLDataType.DECIMAL;
-            case BOOLEAN -> SQLDataType.BOOLEAN;
-            case DATETIME -> SQLDataType.INSTANT;
-        };
-        dataType = dataType.nullable(!required);
-        return DSL.field(column.getValue(), dataType);
-    }
-
 }
