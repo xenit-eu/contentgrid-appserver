be3bfb0a0287f1d557b6baa3bb52580d94b5709f
Roel <roel.schevenels@xenit.eu>
2025-08-28 11:27:35 +0200

    Delete unused EntityNotFoundException

--- contentgrid-appserver-json-schema/src/main/java/com/contentgrid/appserver/json/exceptions/EntityNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.contentgrid.appserver.json.exceptions;
-
-public final class EntityNotFoundException extends InValidJsonException {
-
-    public EntityNotFoundException(String s) {
-        super(s);
-    }
-}
--- contentgrid-appserver-json-schema/src/main/java/com/contentgrid/appserver/json/exceptions/InValidJsonException.java
+++ contentgrid-appserver-json-schema/src/main/java/com/contentgrid/appserver/json/exceptions/InValidJsonException.java
@@ -1,7 +1,7 @@
 package com.contentgrid.appserver.json.exceptions;
 
 public abstract sealed class InValidJsonException extends Exception permits AttributeNotFoundException,
-        EntityNotFoundException, InvalidAttributeTypeException, SchemaValidationException, UnknownFilterTypeException,
+        InvalidAttributeTypeException, SchemaValidationException, UnknownFilterTypeException,
         UnknownFlagException {
 
     protected InValidJsonException(String s) {
