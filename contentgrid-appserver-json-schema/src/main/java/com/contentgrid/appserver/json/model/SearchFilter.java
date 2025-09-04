8801ab390ee679824c28f70af2be5093342f3a37
niels cappelle <niels.cappelle@xenit.eu>
2025-09-04 16:05:57 +0200

    ACC-2229 support OrderedSearchFilter in json schema

--- contentgrid-appserver-json-schema/src/main/java/com/contentgrid/appserver/json/model/SearchFilter.java
+++ contentgrid-appserver-json-schema/src/main/java/com/contentgrid/appserver/json/model/SearchFilter.java
@@ -19,7 +19,7 @@ public class SearchFilter {
     private List<PropertyPathElement> attributePath;
 
     @NonNull
-    private String type; // exact, prefix
+    private String type; // exact, prefix, ...
 
     @JsonInclude(Include.NON_EMPTY)
     private List<String> flags;
