d3bba8a76bfa2f085b03ff36524d27d396c3ebea
niels cappelle <niels.cappelle@xenit.eu>
2025-09-22 17:09:06 +0200

    ACC-2274 move imports from ContentGridRestAutoConfiguration to ContentGridRestConfiguration

--- contentgrid-appserver-autoconfigure/src/main/java/com/contentgrid/appserver/autoconfigure/rest/ContentGridRestAutoConfiguration.java
+++ contentgrid-appserver-autoconfigure/src/main/java/com/contentgrid/appserver/autoconfigure/rest/ContentGridRestAutoConfiguration.java
@@ -3,20 +3,8 @@ package com.contentgrid.appserver.autoconfigure.rest;
 import com.contentgrid.appserver.autoconfigure.domain.ContentGridDomainAutoConfiguration;
 import com.contentgrid.appserver.domain.ContentApi;
 import com.contentgrid.appserver.domain.DatamodelApi;
-import com.contentgrid.appserver.registry.DefaultApplicationNameExtractor;
 import com.contentgrid.appserver.rest.ContentGridRestConfiguration;
 import com.contentgrid.appserver.rest.EntityRestController;
-import com.contentgrid.appserver.rest.ProfileRestController;
-import com.contentgrid.appserver.rest.RootRestController;
-import com.contentgrid.appserver.rest.assembler.EntityDataRepresentationModelAssembler;
-import com.contentgrid.appserver.rest.assembler.profile.hal.ProfileEntityRepresentationModelAssembler;
-import com.contentgrid.appserver.rest.converter.RequestInputDataJacksonModule;
-import com.contentgrid.appserver.rest.converter.UriListHttpMessageConverter;
-import com.contentgrid.appserver.rest.filter.SingleRangeRequestServletFilter;
-import com.contentgrid.appserver.rest.mapping.ContentGridHandlerMappingConfiguration;
-import com.contentgrid.appserver.rest.property.ContentRestController;
-import com.contentgrid.appserver.rest.property.XToManyRelationRestController;
-import com.contentgrid.appserver.rest.property.XToOneRelationRestController;
 import com.contentgrid.thunx.api.autoconfigure.AbacContextAutoConfiguration;
 import com.contentgrid.thunx.api.autoconfigure.JwtAbacAutoConfiguration;
 import com.contentgrid.thunx.spring.data.context.AbacContextSupplier;
@@ -35,22 +23,7 @@ import org.springframework.hateoas.RepresentationModel;
 @ConditionalOnWebApplication(type = Type.SERVLET)
 @ConditionalOnClass({EntityRestController.class, RepresentationModel.class})
 @ConditionalOnBean({DatamodelApi.class, ContentApi.class, AbacContextSupplier.class, ConversionService.class})
-@Import({
-        ContentGridHandlerMappingConfiguration.class,
-        ContentGridRestConfiguration.class,
-        ContentRestController.class,
-        DefaultApplicationNameExtractor.class,
-        EntityDataRepresentationModelAssembler.class,
-        EntityRestController.class,
-        ProfileEntityRepresentationModelAssembler.class,
-        ProfileRestController.class,
-        RequestInputDataJacksonModule.class,
-        RootRestController.class,
-        SingleRangeRequestServletFilter.class,
-        UriListHttpMessageConverter.class,
-        XToManyRelationRestController.class,
-        XToOneRelationRestController.class,
-})
+@Import(ContentGridRestConfiguration.class)
 public class ContentGridRestAutoConfiguration {
 
 }
