bb36110508a5c34561132753015deba6ab87c3b8
niels cappelle <niels.cappelle@xenit.eu>
2025-09-22 10:24:31 +0200

    ACC-2274 fix ContentGridRestAutoConfiguration + add test

--- contentgrid-appserver-autoconfigure/src/main/java/com/contentgrid/appserver/autoconfigure/rest/ContentGridRestAutoConfiguration.java
+++ contentgrid-appserver-autoconfigure/src/main/java/com/contentgrid/appserver/autoconfigure/rest/ContentGridRestAutoConfiguration.java
@@ -17,18 +17,24 @@ import com.contentgrid.appserver.rest.mapping.ContentGridHandlerMappingConfigura
 import com.contentgrid.appserver.rest.property.ContentRestController;
 import com.contentgrid.appserver.rest.property.XToManyRelationRestController;
 import com.contentgrid.appserver.rest.property.XToOneRelationRestController;
+import com.contentgrid.thunx.api.autoconfigure.AbacContextAutoConfiguration;
+import com.contentgrid.thunx.api.autoconfigure.JwtAbacAutoConfiguration;
+import com.contentgrid.thunx.spring.data.context.AbacContextSupplier;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication.Type;
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
 import org.springframework.context.annotation.Import;
+import org.springframework.core.convert.ConversionService;
 import org.springframework.hateoas.RepresentationModel;
 
-@AutoConfiguration(after = ContentGridDomainAutoConfiguration.class)
+@AutoConfiguration(after = {ContentGridDomainAutoConfiguration.class, AbacContextAutoConfiguration.class,
+        JwtAbacAutoConfiguration.class, WebMvcAutoConfiguration.class})
 @ConditionalOnWebApplication(type = Type.SERVLET)
 @ConditionalOnClass({EntityRestController.class, RepresentationModel.class})
-@ConditionalOnBean({DatamodelApi.class, ContentApi.class})
+@ConditionalOnBean({DatamodelApi.class, ContentApi.class, AbacContextSupplier.class, ConversionService.class})
 @Import({
         ContentGridHandlerMappingConfiguration.class,
         ContentGridRestConfiguration.class,
