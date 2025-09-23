58839dc2314de864aa822a226910a58101c2a9bd
niels cappelle <niels.cappelle@xenit.eu>
2025-09-23 11:23:04 +0200

    ACC-2274 address feedback on EncryptedContentStoreAutoConfiguration

--- contentgrid-appserver-autoconfigure/src/main/java/com/contentgrid/appserver/autoconfigure/contentstore/EncryptedContentStoreAutoConfiguration.java
+++ contentgrid-appserver-autoconfigure/src/main/java/com/contentgrid/appserver/autoconfigure/contentstore/EncryptedContentStoreAutoConfiguration.java
@@ -1,5 +1,7 @@
 package com.contentgrid.appserver.autoconfigure.contentstore;
 
+import com.contentgrid.appserver.autoconfigure.contentstore.EncryptedContentStoreAutoConfiguration.EncryptionEngineProperties;
+import com.contentgrid.appserver.autoconfigure.contentstore.EncryptedContentStoreAutoConfiguration.EncryptionKeyWrapperProperties;
 import com.contentgrid.appserver.contentstore.api.ContentStore;
 import com.contentgrid.appserver.contentstore.impl.encryption.EncryptedContentStore;
 import com.contentgrid.appserver.contentstore.impl.encryption.engine.AesCtrEncryptionEngine;
@@ -9,45 +11,91 @@ import com.contentgrid.appserver.contentstore.impl.encryption.keys.DataEncryptio
 import com.contentgrid.appserver.contentstore.impl.encryption.keys.TableStorageDataEncryptionKeyAccessor;
 import com.contentgrid.appserver.contentstore.impl.encryption.keys.UnencryptedSymmetricDataEncryptionKeyWrapper;
 import java.util.List;
+import java.util.Set;
+import lombok.NonNull;
 import org.jooq.DSLContext;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBooleanProperty;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.jooq.JooqAutoConfiguration;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.context.properties.bind.DefaultValue;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
 
 @AutoConfiguration(after = {FileSystemContentStoreAutoConfiguration.class, S3ContentStoreAutoConfiguration.class, JooqAutoConfiguration.class})
 @ConditionalOnClass(EncryptedContentStore.class)
+@ConditionalOnBean(ContentStore.class)
 @ConditionalOnBooleanProperty("contentgrid.appserver.content.encryption.enabled")
+@EnableConfigurationProperties({EncryptionKeyWrapperProperties.class, EncryptionEngineProperties.class})
 public class EncryptedContentStoreAutoConfiguration {
 
     @Bean
+    @ConditionalOnMissingBean
     @ConditionalOnBean(DSLContext.class)
     DataEncryptionKeyAccessor tableStorageEncryptionKeyAccessor(DSLContext dslContext) {
         return new TableStorageDataEncryptionKeyAccessor(dslContext);
     }
 
-    @Bean
-    @ConditionalOnProperty(value = "contentgrid.appserver.content.encryption.wrapper.algorithm", havingValue = "none", matchIfMissing = true)
-    DataEncryptionKeyWrapper unencryptedSymmetricDataEncryptionKeyWrapper() {
-        return new UnencryptedSymmetricDataEncryptionKeyWrapper(true);
-    }
-
-    @Bean
-    @ConditionalOnProperty(value = "contentgrid.appserver.content.encryption.engine.algorithm", havingValue = "AES-CTR", matchIfMissing = true)
-    ContentEncryptionEngine aesCtrEncryptionEngine(@Value("${contentgrid.appserver.content.encryption.engine.key-size-bits:128}") int keySizeBits) {
-        return new AesCtrEncryptionEngine(keySizeBits);
-    }
-
     @Primary
     @Bean
-    @ConditionalOnBean({ContentStore.class, DataEncryptionKeyAccessor.class, DataEncryptionKeyWrapper.class, ContentEncryptionEngine.class})
     ContentStore encryptedContentStore(ContentStore contentStore, DataEncryptionKeyAccessor encryptionKeyAccessor,
-            List<DataEncryptionKeyWrapper> encryptionKeyWrappers, List<ContentEncryptionEngine> encryptionEngines) {
+            List<DataEncryptionKeyWrapper> encryptionKeyWrappers, List<ContentEncryptionEngine> encryptionEngines,
+            EncryptionKeyWrapperProperties encryptionKeyWrapperAlgorithms, EncryptionEngineProperties encryptionEngineProperties) {
+        if (encryptionKeyWrappers.isEmpty()) {
+            encryptionKeyWrappers = encryptionKeyWrapperAlgorithms.algorithms().stream()
+                    .map(this::dataEncryptionKeyWrapperForAlgorithm)
+                    .toList();
+        }
+        if (encryptionEngines.isEmpty()) {
+            encryptionEngines = encryptionEngineProperties.algorithms().stream()
+                    .map(this::contentEncryptionEngineForAlgorithm)
+                    .toList();
+        }
         return new EncryptedContentStore(contentStore, encryptionKeyAccessor, encryptionKeyWrappers, encryptionEngines);
     }
+
+    private DataEncryptionKeyWrapper dataEncryptionKeyWrapperForAlgorithm(String algorithm) {
+        return switch (algorithm.toLowerCase()) {
+            case "none" -> new UnencryptedSymmetricDataEncryptionKeyWrapper(true);
+            default -> throw new UnsupportedOperationException("Data encryption key wrapper algorithm %s not supported".formatted(algorithm));
+        };
+    }
+
+    private ContentEncryptionEngine contentEncryptionEngineForAlgorithm(EncryptionEngineAlgorithmProperties properties) {
+        return switch (properties.type().toLowerCase()) {
+            case "aes-ctr" -> new AesCtrEncryptionEngine(properties.keySizeBits());
+            default -> throw new UnsupportedOperationException("Content encryption algorithm %s not supported".formatted(properties.type()));
+        };
+    }
+
+    @ConfigurationProperties("contentgrid.appserver.content.encryption.wrapper")
+    record EncryptionKeyWrapperProperties(
+            @DefaultValue("none")
+            Set<String> algorithms
+    ) {}
+
+    @ConfigurationProperties("contentgrid.appserver.content.encryption.engine")
+    record EncryptionEngineProperties(
+            Set<EncryptionEngineAlgorithmProperties> algorithms
+    ) {
+        public Set<EncryptionEngineAlgorithmProperties> algorithms() {
+            if (algorithms == null || algorithms.isEmpty()) {
+                // return default value
+                return Set.of(new EncryptionEngineAlgorithmProperties("AES-CTR", 128));
+            } else {
+                return algorithms;
+            }
+        }
+    }
+
+    record EncryptionEngineAlgorithmProperties(
+            @NonNull
+            String type,
+            @DefaultValue("128")
+            int keySizeBits
+    ) {}
 }
--- contentgrid-appserver-autoconfigure/src/test/java/com/contentgrid/appserver/autoconfigure/contentstore/EncryptedContentStoreAutoConfigurationTest.java
+++ contentgrid-appserver-autoconfigure/src/test/java/com/contentgrid/appserver/autoconfigure/contentstore/EncryptedContentStoreAutoConfigurationTest.java
@@ -3,6 +3,16 @@ package com.contentgrid.appserver.autoconfigure.contentstore;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import com.contentgrid.appserver.contentstore.impl.encryption.EncryptedContentStore;
+import com.contentgrid.appserver.contentstore.impl.encryption.engine.ContentEncryptionEngine;
+import com.contentgrid.appserver.contentstore.impl.encryption.keys.DataEncryptionKeyAccessor;
+import com.contentgrid.appserver.contentstore.impl.encryption.keys.DataEncryptionKeyWrapper;
+import com.contentgrid.appserver.contentstore.impl.encryption.keys.TableStorageDataEncryptionKeyAccessor;
+import com.contentgrid.appserver.contentstore.impl.encryption.keys.UnencryptedSymmetricDataEncryptionKeyWrapper;
+import com.contentgrid.appserver.contentstore.impl.encryption.keys.WrappingKeyId;
+import com.contentgrid.appserver.contentstore.impl.encryption.testing.InMemoryDataEncryptionKeyAccessor;
+import com.contentgrid.appserver.contentstore.impl.encryption.testing.XorTestEncryptionEngine;
+import com.contentgrid.appserver.contentstore.impl.fs.FilesystemContentStore;
+import java.util.Set;
 import org.jooq.DSLContext;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -16,6 +26,8 @@ import org.springframework.boot.convert.ApplicationConversionService;
 import org.springframework.boot.logging.LogLevel;
 import org.springframework.boot.test.context.FilteredClassLoader;
 import org.springframework.boot.test.context.runner.WebApplicationContextRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 class EncryptedContentStoreAutoConfigurationTest {
 
@@ -46,15 +58,26 @@ class EncryptedContentStoreAutoConfigurationTest {
         contextRunner
                 .withClassLoader(new FilteredClassLoader(DSLContext.class))
                 .run(context -> {
+                    assertThat(context).hasFailed();
+                });
+    }
+
+    @Test
+    void checkWithMissingContentStore() {
+        // no ContentStore delegate
+        contextRunner
+                .withClassLoader(new FilteredClassLoader(FilesystemContentStore.class))
+                .run(context -> {
                     assertThat(context).hasNotFailed();
                     assertThat(context).doesNotHaveBean(EncryptedContentStore.class);
+                    assertThat(context).doesNotHaveBean(TableStorageDataEncryptionKeyAccessor.class);
                 });
     }
 
     @Test
     void checkWithValidWrapperAlgorithm() {
         contextRunner
-                .withPropertyValues("contentgrid.appserver.content.encryption.wrapper.algorithm=none")
+                .withPropertyValues("contentgrid.appserver.content.encryption.wrapper.algorithms[0]=none")
                 .run(context -> {
                     assertThat(context).hasNotFailed();
                     assertThat(context).hasSingleBean(EncryptedContentStore.class);
@@ -64,10 +87,9 @@ class EncryptedContentStoreAutoConfigurationTest {
     @Test
     void checkWithInvalidWrapperAlgorithm() {
         contextRunner
-                .withPropertyValues("contentgrid.appserver.content.encryption.wrapper.algorithm=unknown")
+                .withPropertyValues("contentgrid.appserver.content.encryption.wrapper.algorithms[0]=unknown")
                 .run(context -> {
-                    assertThat(context).hasNotFailed();
-                    assertThat(context).doesNotHaveBean(EncryptedContentStore.class);
+                    assertThat(context).hasFailed();
                 });
     }
 
@@ -76,8 +98,8 @@ class EncryptedContentStoreAutoConfigurationTest {
     void checkWithValidEncryptionEngineKeySizeBits(int keySizeBits) {
         contextRunner
                 .withPropertyValues(
-                        "contentgrid.appserver.content.encryption.engine.algorithm=AES-CTR",
-                        "contentgrid.appserver.content.encryption.engine.key-size-bits=" + keySizeBits
+                        "contentgrid.appserver.content.encryption.engine.algorithms[0].type=AES-CTR",
+                        "contentgrid.appserver.content.encryption.engine.algorithms[0].keySizeBits=" + keySizeBits
                 )
                 .run(context -> {
                     assertThat(context).hasNotFailed();
@@ -90,8 +112,8 @@ class EncryptedContentStoreAutoConfigurationTest {
     void checkWithInvalidEncryptionEngineKeySizeBits(int keySizeBits) {
         contextRunner
                 .withPropertyValues(
-                        "contentgrid.appserver.content.encryption.engine.algorithm=AES-CTR",
-                        "contentgrid.appserver.content.encryption.engine.key-size-bits=" + keySizeBits
+                        "contentgrid.appserver.content.encryption.engine.algorithms[0].type=AES-CTR",
+                        "contentgrid.appserver.content.encryption.engine.algorithms[0].keySizeBits=" + keySizeBits
                 )
                 .run(context -> {
                     assertThat(context).hasFailed();
@@ -99,13 +121,101 @@ class EncryptedContentStoreAutoConfigurationTest {
     }
 
     @Test
-    void checkWithUnknownEncryptionEngineKeyAlgorithm() {
+    void checkWithMultipleEncryptionKeyAlgorithms() {
         contextRunner
-                .withPropertyValues("contentgrid.appserver.content.encryption.engine.algorithm=unknown")
+                .withPropertyValues(
+                        "contentgrid.appserver.content.encryption.engine.algorithms[0].type=AES-CTR",
+                        "contentgrid.appserver.content.encryption.engine.algorithms[0].keySizeBits=128",
+                        "contentgrid.appserver.content.encryption.engine.algorithms[1].type=AES-CTR",
+                        "contentgrid.appserver.content.encryption.engine.algorithms[1].keySizeBits=192",
+                        "contentgrid.appserver.content.encryption.engine.algorithms[2].type=AES-CTR",
+                        "contentgrid.appserver.content.encryption.engine.algorithms[2].keySizeBits=256"
+                )
                 .run(context -> {
                     assertThat(context).hasNotFailed();
-                    assertThat(context).doesNotHaveBean(EncryptedContentStore.class);
+                    assertThat(context).hasSingleBean(EncryptedContentStore.class);
                 });
+    }
+
+    @Test
+    void checkWithUnknownEncryptionEngineKeyAlgorithm() {
+        contextRunner
+                .withPropertyValues("contentgrid.appserver.content.encryption.engine.algorithms[0].type=unknown")
+                .run(context -> {
+                    assertThat(context).hasFailed();
+                });
+    }
+
+    @Test
+    void checkWithMissingEncryptionEngineKeyAlgorithm() {
+        contextRunner
+                .withPropertyValues("contentgrid.appserver.content.encryption.engine.algorithms[0].keySizeBits=128")
+                .run(context -> {
+                    assertThat(context).hasFailed();
+                });
+    }
+
+    @Test
+    void checkWithCustomEncryptionKeyWrapper() {
+        contextRunner
+                .withUserConfiguration(CustomEncryptionKeyWrapperConfiguration.class)
+                .run(context -> {
+                    assertThat(context).hasNotFailed();
+                    assertThat(context).hasSingleBean(EncryptedContentStore.class);
+                });
+    }
+
+    @Test
+    void checkWithCustomEncryptionEngine() {
+        contextRunner
+                .withUserConfiguration(CustomEncryptionEngineConfiguration.class)
+                .run(context -> {
+                    assertThat(context).hasNotFailed();
+                    assertThat(context).hasSingleBean(EncryptedContentStore.class);
+                });
+    }
+
+    @Test
+    void checkWithCustomEncryptionKeyAccessor() {
+        contextRunner
+                .withUserConfiguration(CustomEncryptionKeyAccessorConfiguration.class)
+                .run(context -> {
+                    assertThat(context).hasNotFailed();
+                    assertThat(context).hasSingleBean(EncryptedContentStore.class);
+                    assertThat(context).doesNotHaveBean(TableStorageDataEncryptionKeyAccessor.class);
+                });
+    }
+
+    @Configuration
+    static class CustomEncryptionKeyWrapperConfiguration {
+
+        @Bean
+        DataEncryptionKeyWrapper customEncryptionKeyWrapper() {
+            return new UnencryptedSymmetricDataEncryptionKeyWrapper(true) {
+                @Override
+                public Set<WrappingKeyId> getSupportedKeyIds() {
+                    return Set.of(WrappingKeyId.of("test"));
+                }
+            };
+        }
+    }
+
+    @Configuration
+    static class CustomEncryptionEngineConfiguration {
+
+        @Bean
+        ContentEncryptionEngine customEncryptionEngine() {
+            return new XorTestEncryptionEngine();
+        }
+    }
+
+    @Configuration
+    static class CustomEncryptionKeyAccessorConfiguration {
+
+        @Bean
+        DataEncryptionKeyAccessor customEncryptionKeyAccessor() {
+            return new InMemoryDataEncryptionKeyAccessor();
+        }
     }
 
 }
