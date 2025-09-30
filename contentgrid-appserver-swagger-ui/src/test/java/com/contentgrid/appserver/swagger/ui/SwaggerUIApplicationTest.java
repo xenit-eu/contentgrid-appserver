2af917157e40415915175e5476870448d9b2187b
Roel <roel.schevenels@xenit.eu>
2025-09-30 16:19:28 +0200

    Add swagger-ui subproject and autoconfiguration [ACC-2295]

---
+++ contentgrid-appserver-swagger-ui/src/test/java/com/contentgrid/appserver/swagger/ui/SwaggerUIApplicationTest.java
@@ -0,0 +1,52 @@
+package com.contentgrid.appserver.swagger.ui;
+
+import static org.hamcrest.Matchers.not;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+
+@WebMvcTest
+@ContextConfiguration(classes = SwaggerUIInitializerController.class)
+@AutoConfigureMockMvc(printOnlyOnFailure = false)
+public class SwaggerUIApplicationTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    void webjarsSwaggerUIwithoutVersionReturnsHttpOk() throws Exception {
+        this.mockMvc.perform(get("/webjars/swagger-ui/index.html"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.TEXT_HTML));
+
+    }
+
+    @Test
+    void webjarsSwaggerUIwithInvalidVersionReturnsHttpNotFound() throws Exception {
+        this.mockMvc.perform(get("/webjars/swagger-ui/1.2.3.4/index.html"))
+                .andExpect(MockMvcResultMatchers.status().isNotFound());
+    }
+
+    @Test
+    void customSwaggerInitializer() throws Exception {
+        this.mockMvc.perform(get("/webjars/swagger-ui/swagger-initializer.js"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+
+                // should NOT serve any default config
+                .andExpect(MockMvcResultMatchers.content().string(not(Matchers.containsString("petstore"))))
+
+                // but use the contentgrid url: "/openapi.yml"
+                .andExpect(MockMvcResultMatchers.content().string(Matchers.containsString("url: \"/openapi.yml\"")));
+    }
+
+
+}
