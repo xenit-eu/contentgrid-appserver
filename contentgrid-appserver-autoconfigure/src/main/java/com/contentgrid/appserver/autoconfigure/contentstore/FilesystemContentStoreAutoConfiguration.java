73321fd1f98c30908e90a3f0d1956a8b007042a5
niels cappelle <niels.cappelle@xenit.eu>
2025-10-03 10:21:53 +0200

    Create directory if provided 'contentgrid.appserver.content.fs.path' does not exist

--- contentgrid-appserver-autoconfigure/src/main/java/com/contentgrid/appserver/autoconfigure/contentstore/FilesystemContentStoreAutoConfiguration.java
+++ contentgrid-appserver-autoconfigure/src/main/java/com/contentgrid/appserver/autoconfigure/contentstore/FilesystemContentStoreAutoConfiguration.java
@@ -5,9 +5,11 @@ import com.contentgrid.appserver.contentstore.impl.fs.FilesystemContentStore;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.attribute.FileAttribute;
 import java.nio.file.attribute.PosixFilePermission;
 import java.nio.file.attribute.PosixFilePermissions;
 import java.util.EnumSet;
+import java.util.Set;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
@@ -19,24 +21,24 @@ import org.springframework.context.annotation.Bean;
 @ConditionalOnClass(FilesystemContentStore.class)
 public class FilesystemContentStoreAutoConfiguration {
 
+    private static final FileAttribute<Set<PosixFilePermission>> PERMISSIONS = PosixFilePermissions.asFileAttribute(
+            EnumSet.of(PosixFilePermission.OWNER_READ, PosixFilePermission.OWNER_WRITE, PosixFilePermission.OWNER_EXECUTE)
+    );
+
     @Bean
     @ConditionalOnMissingBean
     @ConditionalOnProperty(value = "contentgrid.appserver.content-store.type", havingValue = "ephemeral", matchIfMissing = true)
     public ContentStore ephemeralContentStore() throws IOException {
-        var permissions = EnumSet.of(
-                PosixFilePermission.OWNER_READ, PosixFilePermission.OWNER_WRITE, PosixFilePermission.OWNER_EXECUTE
-        );
-        return new FilesystemContentStore(Files.createTempDirectory("contentgrid", PosixFilePermissions.asFileAttribute(permissions)));
+        return new FilesystemContentStore(Files.createTempDirectory("contentgrid", PERMISSIONS));
     }
 
     @Bean
     @ConditionalOnMissingBean
     @ConditionalOnProperty(value = "contentgrid.appserver.content-store.type", havingValue = "fs")
-    public ContentStore filesystemContentStore(@Value("${contentgrid.appserver.content.fs.path}") Path path) {
-        if (Files.isDirectory(path)) {
-            return new FilesystemContentStore(path);
-        } else {
-            throw new IllegalStateException("directory %s does not exist".formatted(path));
+    public ContentStore filesystemContentStore(@Value("${contentgrid.appserver.content.fs.path:}") Path path) throws IOException {
+        if (path == null || path.toString().isBlank()) {
+            throw new IllegalArgumentException("Property 'contentgrid.appserver.content.fs.path' is required when 'contentgrid.appserver.content-store.type' is 'fs'");
         }
+        return new FilesystemContentStore(Files.createDirectories(path, PERMISSIONS));
     }
 }
