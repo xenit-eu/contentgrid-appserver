9690ad78ac7db6327f407f4d19a18b2e69118fb5
Pieter-Jan Vrielynck <43749206+pvriel@users.noreply.github.com>
2025-10-09 10:15:46 +0200

    [ACC-2213] add FTS SQL DSL
    # Conflicts:
    #	contentgrid-appserver-query-engine-impl-jooq/build.gradle

--- contentgrid-appserver-query-engine-impl-jooq/src/main/java/com/contentgrid/appserver/query/engine/jooq/JOOQThunkExpressionVisitor.java
+++ contentgrid-appserver-query-engine-impl-jooq/src/main/java/com/contentgrid/appserver/query/engine/jooq/JOOQThunkExpressionVisitor.java
@@ -12,6 +12,7 @@ import com.contentgrid.appserver.application.model.values.AttributeName;
 import com.contentgrid.appserver.application.model.values.RelationName;
 import com.contentgrid.appserver.application.model.values.TableName;
 import com.contentgrid.appserver.query.engine.api.exception.InvalidThunkExpressionException;
+import com.contentgrid.appserver.query.engine.api.thunx.expression.StringComparison;
 import com.contentgrid.appserver.query.engine.api.thunx.expression.StringComparison.ContentGridPrefixSearch;
 import com.contentgrid.thunx.predicates.model.FunctionExpression;
 import com.contentgrid.thunx.predicates.model.Scalar;
@@ -181,6 +182,10 @@ public class JOOQThunkExpressionVisitor implements ThunkExpressionVisitor<Field<
                     var leftField = prefixSearchNormalize(left);
                     var rightField = prefixSearchNormalize(right);
                     yield leftField.startsWith(rightField);
+                } else if (functionExpression instanceof StringComparison.ContentGridFullTextSearch contentGridFullTextSearch) {
+                    var left = contentGridFullTextSearch.getLeftTerm().accept(this, context);
+                    var right = contentGridFullTextSearch.getRightTerm().accept(this, context);
+                    yield DSL.condition("? &&& ?", left, DSL.inline(right));
                 } else {
                     throw new InvalidThunkExpressionException(
                             "Function expression with type %s is not supported.".formatted(
