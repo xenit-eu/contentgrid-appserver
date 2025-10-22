276b97351771b9b6782c9dc01d35301f7492f243
Pieter-Jan Vrielynck <43749206+pvriel@users.noreply.github.com>
2025-10-22 14:40:16 +0200

    [ACC-2213-fts] allow custom DB image for appserver
    # Conflicts:
    #	contentgrid-appserver-query-engine-impl-jooq/build.gradle

--- contentgrid-appserver-query-engine-impl-jooq/src/main/java/com/contentgrid/appserver/query/engine/jooq/JOOQQueryEngine.java
+++ contentgrid-appserver-query-engine-impl-jooq/src/main/java/com/contentgrid/appserver/query/engine/jooq/JOOQQueryEngine.java
@@ -79,7 +79,6 @@ import java.util.stream.Stream;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
-import org.jetbrains.annotations.NotNull;
 import org.jooq.Condition;
 import org.jooq.DSLContext;
 import org.jooq.Field;
@@ -195,7 +194,7 @@ public class JOOQQueryEngine implements QueryEngine {
                 .map(checkVersionSatisfied(entityRequest));
     }
 
-    private static @NotNull Function<EntityData, EntityData> checkVersionSatisfied(@NotNull EntityRequest entityRequest) {
+    private static Function<EntityData, EntityData> checkVersionSatisfied(EntityRequest entityRequest) {
         return entityData -> {
             if (!entityRequest.getVersionConstraint().isSatisfiedBy(entityData.getIdentity().getVersion())) {
                 throw new UnsatisfiedVersionException(
