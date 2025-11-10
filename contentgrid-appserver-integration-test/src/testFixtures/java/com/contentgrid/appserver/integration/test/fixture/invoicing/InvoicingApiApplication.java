21775c145ba2426a9fd3ae825438e942ec4b90d0
niels cappelle <niels.cappelle@xenit.eu>
2025-11-10 10:36:21 +0100

    ACC-2401 add testFixtures model from contentgrid-spring

---
+++ contentgrid-appserver-integration-test/src/testFixtures/java/com/contentgrid/appserver/integration/test/fixture/invoicing/InvoicingApiApplication.java
@@ -0,0 +1,13 @@
+package com.contentgrid.appserver.integration.test.fixture.invoicing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvoicingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvoicingApiApplication.class, args);
+	}
+
+}
