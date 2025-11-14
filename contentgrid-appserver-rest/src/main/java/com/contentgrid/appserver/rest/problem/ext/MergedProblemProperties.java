09d7b85fe7a7882573fd05ba85a6077e50832b91
Lars Vierbergen <lars.vierbergen@xenit.eu>
2025-11-14 14:51:45 +0100

    [ACC-2378] Fix MergedProblemProperties issues with Map being present when it's being extended.
    Because `Map` doesn't get unwrapped by `@JsonUnwrapped`, we use a wrapper class that knows to use `@JsonAnyGetter` to flatten the map into the problem object.

--- contentgrid-appserver-rest/src/main/java/com/contentgrid/appserver/rest/problem/ext/MergedProblemProperties.java
+++ contentgrid-appserver-rest/src/main/java/com/contentgrid/appserver/rest/problem/ext/MergedProblemProperties.java
@@ -1,23 +1,37 @@
 package com.contentgrid.appserver.rest.problem.ext;
 
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.fasterxml.jackson.annotation.JsonUnwrapped;
+import java.util.Map;
 import java.util.Optional;
 import java.util.function.UnaryOperator;
 import lombok.AccessLevel;
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+import lombok.Value;
+import lombok.experimental.Delegate;
 import org.springframework.hateoas.mediatype.problem.Problem;
 import org.springframework.hateoas.mediatype.problem.Problem.ExtendedProblem;
 
 @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+@JsonInclude(Include.NON_EMPTY)
 public class MergedProblemProperties {
-    @JsonUnwrapped
-    @JsonInclude(Include.NON_NULL)
     private final Object original;
+    private final Object extension;
 
     @JsonUnwrapped
-    private final Object extension;
+    @JsonInclude(Include.NON_NULL)
+    public Object getOriginal() {
+        return MapWrapper.wrap(original);
+    }
+
+    @JsonUnwrapped
+    public Object getExtension() {
+        return MapWrapper.wrap(extension);
+    }
 
     public static Problem extend(Problem problem, Object... extensions) {
         var properties = create(problem);
@@ -65,6 +79,24 @@ public class MergedProblemProperties {
             return mergedProblemProperties.findExtension(type);
         } else {
             return Optional.empty();
+        }
+    }
+
+    /**
+     * This wraps a plain {@link Map}, because jackson does not know how to apply {@link JsonUnwrapped} on a map.
+     * To flatten all the map key-value pairs into the object, you need {@link JsonAnyGetter} instead, which this class does.
+     */
+    @Value
+    private static class MapWrapper {
+        @JsonAnyGetter
+        @NonNull
+        Map map;
+
+        private static Object wrap(Object object) {
+            if(object instanceof Map map) {
+                return new MapWrapper(map);
+            }
+            return object;
         }
     }
 
