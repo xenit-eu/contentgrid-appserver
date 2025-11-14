ead1c7178afa19577c005609b47aa63fa26626b7
niels cappelle <niels.cappelle@xenit.eu>
2025-11-14 16:51:32 +0100

    ACC-2401 use separate database per test

--- contentgrid-appserver-integration-test/src/testFixtures/java/com/contentgrid/appserver/integration/test/fixture/invoicing/InvoicingApiApplication.java
+++ contentgrid-appserver-integration-test/src/testFixtures/java/com/contentgrid/appserver/integration/test/fixture/invoicing/InvoicingApiApplication.java
@@ -2,6 +2,11 @@ package com.contentgrid.appserver.integration.test.fixture.invoicing;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.containers.RabbitMQContainer;
 
 @SpringBootApplication
 public class InvoicingApiApplication {
@@ -9,5 +14,21 @@ public class InvoicingApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(InvoicingApiApplication.class, args);
 	}
+
+    @TestConfiguration(proxyBeanMethods = false)
+    static class TestDatabaseConfiguration {
+
+        @Bean
+        @ServiceConnection
+        PostgreSQLContainer<?> postgreSQLContainer() {
+            return new PostgreSQLContainer<>("postgres:15");
+        }
+
+        @Bean
+        @ServiceConnection
+        RabbitMQContainer rabbitMQContainer() {
+            return new RabbitMQContainer("rabbitmq:4");
+        }
+    }
 
 }
