08c9e05254595d3288bf6138d5a82a0fa9a7af4d
niels cappelle <niels.cappelle@xenit.eu>
2026-01-22 12:02:37 +0100

    ACC-2559 fix JOOQQueryEngineTest

--- contentgrid-appserver-query-engine-impl-jooq/src/test/java/com/contentgrid/appserver/query/engine/jooq/JOOQQueryEngineTest.java
+++ contentgrid-appserver-query-engine-impl-jooq/src/test/java/com/contentgrid/appserver/query/engine/jooq/JOOQQueryEngineTest.java
@@ -1494,9 +1494,18 @@ class JOOQQueryEngineTest {
                                         .build())
                                 .build())
                         .build(),
+                // Empty attributes in EntityData
                 EntityData.builder()
                         .name(PERSON.getName())
                         .id(BOB_ID)
+                        .build(),
+                // Empty attributes in CompositeAttributeData
+                EntityData.builder()
+                        .name(INVOICE.getName())
+                        .id(INVOICE1_ID)
+                        .attribute(CompositeAttributeData.builder()
+                                .name(INVOICE_CONTENT.getName())
+                                .build())
                         .build()
         );
     }
@@ -1612,14 +1621,6 @@ class JOOQQueryEngineTest {
                         .attribute(SimpleAttributeData.builder()
                                 .name(INVOICE_IS_PAID.getName())
                                 .value("invalid_boolean") // String instead of boolean
-                                .build())
-                        .build()),
-                Arguments.argumentSet("No SimpleAttributeData provided in CompositeAttributeData", IllegalInputDataException.class,
-                        EntityData.builder()
-                        .name(INVOICE.getName())
-                        .id(INVOICE1_ID)
-                        .attribute(CompositeAttributeData.builder()
-                                .name(INVOICE_CONTENT.getName()) // No attributes
                                 .build())
                         .build())
                 // TODO: ACC-2051: provide audit_metadata/readonly attribute
