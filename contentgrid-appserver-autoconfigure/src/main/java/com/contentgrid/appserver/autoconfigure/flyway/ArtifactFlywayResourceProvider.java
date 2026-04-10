06112c2f527399144da546b55659f5e42f6494b4
niels cappelle <niels.cappelle@xenit.eu>
2026-04-10 15:19:08 +0200

    ACC-2745 let flyway migrations be resolved from an Artifact

---
+++ contentgrid-appserver-autoconfigure/src/main/java/com/contentgrid/appserver/autoconfigure/flyway/ArtifactFlywayResourceProvider.java
@@ -0,0 +1,74 @@
+package com.contentgrid.appserver.autoconfigure.flyway;
+
+import com.contentgrid.appserver.infrastructure.api.Artifact;
+import com.contentgrid.appserver.infrastructure.api.ArtifactEntry;
+import com.contentgrid.appserver.infrastructure.api.ArtifactEntryNotFoundException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.nio.file.Path;
+import java.util.Collection;
+import java.util.stream.Stream;
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import org.flywaydb.core.api.ResourceProvider;
+import org.flywaydb.core.api.resource.LoadableResource;
+
+@RequiredArgsConstructor
+public class ArtifactFlywayResourceProvider implements ResourceProvider {
+
+    private final Artifact artifact;
+
+    @Override
+    @SneakyThrows
+    public LoadableResource getResource(String name) {
+        try {
+            return new ArtifactEntryLoadableResource(artifact.load(Path.of(name)));
+        } catch (ArtifactEntryNotFoundException e) {
+            return null;
+        }
+    }
+
+    @Override
+    @SneakyThrows
+    public Collection<LoadableResource> getResources(String prefix, String[] suffixes) {
+        return artifact.loadAll(Path.of(".")).stream()
+                .filter(entry -> entry.getEntryReference().getFilename().startsWith(prefix))
+                .filter(entry -> Stream.of(suffixes)
+                        .anyMatch(suffix -> entry.getEntryReference().getFilename().endsWith(suffix)))
+                .map(ArtifactEntryLoadableResource::new)
+                .map(LoadableResource.class::cast)
+                .toList();
+    }
+
+    @RequiredArgsConstructor
+    public static class ArtifactEntryLoadableResource extends LoadableResource {
+
+        private final ArtifactEntry entry;
+
+        @Override
+        @SneakyThrows
+        public Reader read() {
+            return new InputStreamReader(entry.getInputStream());
+        }
+
+        @Override
+        public String getAbsolutePath() {
+            return entry.getEntryReference().getAbsolutePath();
+        }
+
+        @Override
+        public String getAbsolutePathOnDisk() {
+            return entry.getEntryReference().getAbsolutePath();
+        }
+
+        @Override
+        public String getFilename() {
+            return entry.getEntryReference().getFilename();
+        }
+
+        @Override
+        public String getRelativePath() {
+            return entry.getEntryReference().getRelativePath();
+        }
+    }
+}
---
+++ contentgrid-appserver-autoconfigure/src/main/java/com/contentgrid/appserver/autoconfigure/flyway/FlywayPostgresAutoConfiguration.java
@@ -0,0 +1,22 @@
+package com.contentgrid.appserver.autoconfigure.flyway;
+
+import com.contentgrid.appserver.infrastructure.api.Artifact;
+import java.nio.file.Path;
+import org.flywaydb.core.Flyway;
+import org.flywaydb.core.api.resource.LoadableResource;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.flyway.FlywayConfigurationCustomizer;
+import org.springframework.context.annotation.Bean;
+
+@AutoConfiguration
+@ConditionalOnClass(Flyway.class)
+public class FlywayPostgresAutoConfiguration {
+
+    @Bean
+    @ConditionalOnClass(LoadableResource.class)
+    FlywayConfigurationCustomizer infrastructureResourceProviderFlywayConfigurationCustomizer(Artifact artifact) {
+        var resourceProvider = new ArtifactFlywayResourceProvider(artifact.subDir(Path.of("db", "migration")));
+        return configuration -> configuration.resourceProvider(resourceProvider);
+    }
+}
