fa38f47b89a1085fdc0d55596dfd99475e9ebf2a
niels cappelle <niels.cappelle@xenit.eu>
2026-04-29 17:38:20 +0200

    ACC-2745 add ArtifactAutomationsModelResolver

--- contentgrid-appserver-domain/src/test/java/com/contentgrid/appserver/domain/automations/ArtifactAutomationsModelResolverTest.java
+++ contentgrid-appserver-domain/src/test/java/com/contentgrid/appserver/domain/automations/ArtifactAutomationsModelResolverTest.java
@@ -3,18 +3,30 @@ package com.contentgrid.appserver.domain.automations;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import com.contentgrid.appserver.application.model.Application;
+import com.contentgrid.appserver.application.model.Entity;
+import com.contentgrid.appserver.application.model.attributes.ContentAttribute;
+import com.contentgrid.appserver.application.model.values.ApplicationName;
+import com.contentgrid.appserver.application.model.values.AttributeName;
+import com.contentgrid.appserver.application.model.values.ColumnName;
+import com.contentgrid.appserver.application.model.values.EntityName;
+import com.contentgrid.appserver.application.model.values.LinkName;
+import com.contentgrid.appserver.application.model.values.PathSegmentName;
+import com.contentgrid.appserver.application.model.values.TableName;
 import com.contentgrid.appserver.domain.automations.AutomationsModel.AutomationAnnotationModel;
 import com.contentgrid.appserver.domain.automations.AutomationsModel.AutomationModel;
+import com.contentgrid.appserver.infrastructure.api.Artifact;
 import com.contentgrid.appserver.infrastructure.api.ArtifactEntry;
 import com.contentgrid.appserver.infrastructure.api.ArtifactEntryUnreadableException;
 import com.contentgrid.appserver.infrastructure.impl.fs.classpath.ClassPathArtifact;
 import java.nio.file.Path;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
-class AutomationsModelTest {
+class ArtifactAutomationsModelResolverTest {
 
     private static final String AUTOMATION_ID = "a0da9322-3dae-4b5a-8f92-c3cb53989938";
     private static final String SYSTEM_ID = "my-system";
@@ -49,19 +61,54 @@ class AutomationsModelTest {
             ))
             .build();
 
+    private static final Application APPLICATION_MODEL = Application.builder()
+            .name(ApplicationName.of("test-application"))
+            .entity(Entity.builder()
+                    .name(EntityName.of("invoice"))
+                    .table(TableName.of("invoice"))
+                    .linkName(LinkName.of("invoice"))
+                    .pathSegment(PathSegmentName.of("invoices"))
+                    .attribute(ContentAttribute.builder()
+                            .name(AttributeName.of("content"))
+                            .linkName(LinkName.of("content"))
+                            .pathSegment(PathSegmentName.of("content"))
+                            .idColumn(ColumnName.of("content__id"))
+                            .filenameColumn(ColumnName.of("content__filename"))
+                            .mimetypeColumn(ColumnName.of("content__mimetype"))
+                            .lengthColumn(ColumnName.of("content__length"))
+                            .build())
+                    .build())
+            .build();
+
     @Test
-    void loadConfigUnreadable() throws Exception {
-        var artifactEntry = Mockito.mock(ArtifactEntry.class);
-        Mockito.doThrow(ArtifactEntryUnreadableException.class)
-                .when(artifactEntry).getInputStream();
-        assertThatThrownBy(() -> AutomationsModel.fromConfig(artifactEntry))
-                .isInstanceOf(IllegalStateException.class);
+    void resolveConfig() {
+        var artifact = new ClassPathArtifact(ArtifactAutomationsModelResolverTest.class.getClassLoader(), Path.of(""));
+
+        var resolver = new ArtifactAutomationsModelResolver(artifact);
+
+        assertThat(resolver.resolve(APPLICATION_MODEL)).isEqualTo(MODEL);
     }
 
     @Test
-    void loadFileConfig() throws Exception {
-        var artifact = new ClassPathArtifact(AutomationsModelTest.class.getClassLoader(), Path.of(""));
-        var artifactEntry = artifact.loadRequired(Path.of("automations.json"));
-        assertThat(AutomationsModel.fromConfig(artifactEntry)).isEqualTo(MODEL);
+    void resolveConfigNotFound() {
+        var artifact = new ClassPathArtifact(ArtifactAutomationsModelResolverTest.class.getClassLoader(), Path.of("nonexisting"));
+
+        var resolver = new ArtifactAutomationsModelResolver(artifact);
+
+        assertThat(resolver.resolve(APPLICATION_MODEL).getAutomations()).isEmpty();
+    }
+
+    @Test
+    void resolveConfigUnreadable() throws Exception {
+        var artifact = Mockito.mock(Artifact.class);
+        var artifactEntry = Mockito.mock(ArtifactEntry.class);
+        Mockito.doThrow(ArtifactEntryUnreadableException.class)
+                .when(artifactEntry).getInputStream();
+        Mockito.doReturn(Optional.of(artifactEntry)).when(artifact).load(Mockito.any());
+
+        var resolver = new ArtifactAutomationsModelResolver(artifact);
+
+        assertThatThrownBy(() -> resolver.resolve(APPLICATION_MODEL))
+                .isInstanceOf(IllegalStateException.class);
     }
 }
