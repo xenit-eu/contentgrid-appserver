734d94403b663bc16850a3d9c5d1f567aff48271
niels cappelle <niels.cappelle@xenit.eu>
2026-05-04 13:44:35 +0200

    ACC-2745 reuse resource in OpenApiController

--- contentgrid-appserver-webjars/src/main/java/com/contentgrid/appserver/webjars/WebjarsRestConfiguration.java
+++ contentgrid-appserver-webjars/src/main/java/com/contentgrid/appserver/webjars/WebjarsRestConfiguration.java
@@ -4,7 +4,6 @@ import com.contentgrid.appserver.infrastructure.api.Artifact;
 import com.contentgrid.appserver.webjars.hal.explorer.HalExplorerController;
 import com.contentgrid.appserver.webjars.swagger.ui.OpenApiController;
 import com.contentgrid.appserver.webjars.swagger.ui.SwaggerUIInitializerController;
-import java.nio.file.Path;
 import lombok.SneakyThrows;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,8 +16,7 @@ public class WebjarsRestConfiguration {
     @Bean
     @SneakyThrows
     OpenApiController openApiController(Artifact artifact) {
-        var entry = artifact.load(Path.of("META-INF", "resources", "openapi.yml")).orElse(null);
-        return new OpenApiController(entry);
+        return new OpenApiController(artifact);
     }
 
 }
