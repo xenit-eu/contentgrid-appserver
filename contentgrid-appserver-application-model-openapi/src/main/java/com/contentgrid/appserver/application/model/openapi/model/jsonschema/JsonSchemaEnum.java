59df6d50ad3bcbdd1c589735257d877a7367288b
Lars Vierbergen <lars.vierbergen@xenit.eu>
2026-05-07 10:39:18 +0200

    [ACC-2678] Add examples back to enums.
    The example just takes the first enum value as option

--- contentgrid-appserver-application-model-openapi/src/main/java/com/contentgrid/appserver/application/model/openapi/model/jsonschema/JsonSchemaEnum.java
+++ contentgrid-appserver-application-model-openapi/src/main/java/com/contentgrid/appserver/application/model/openapi/model/jsonschema/JsonSchemaEnum.java
@@ -1,7 +1,9 @@
 package com.contentgrid.appserver.application.model.openapi.model.jsonschema;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import java.util.List;
+import java.util.Objects;
 import lombok.AccessLevel;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -11,6 +13,7 @@ import lombok.Value;
 @EqualsAndHashCode(callSuper = true)
 @Value
 @ToString(callSuper = true)
+@JsonPropertyOrder({"type", "format", "title", "description", "enum", "examples"})
 public class JsonSchemaEnum extends JsonSchemaString {
 
     @Getter(AccessLevel.NONE)
@@ -19,5 +22,14 @@ public class JsonSchemaEnum extends JsonSchemaString {
     @JsonProperty("enum")
     public List<String> getEnum() {
         return this.enum_;
+    }
+
+    @Override
+    public List<Object> getExamples() {
+        return enum_.stream()
+                .filter(Objects::nonNull)
+                .findFirst()
+                .<List<Object>>map(List::of)
+                .orElseGet(List::of);
     }
 }
