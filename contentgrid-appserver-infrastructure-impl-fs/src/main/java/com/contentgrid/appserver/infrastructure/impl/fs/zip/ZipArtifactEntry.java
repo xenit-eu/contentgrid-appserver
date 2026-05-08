04476d57f424a4ec604a769794329433ec59d26e
Thijs Lemmens <thijs.lemmens@xenit.eu>
2026-05-08 10:03:26 +0200

    ACC-2745 Fix sonar remark about unclosed resource

--- contentgrid-appserver-infrastructure-impl-fs/src/main/java/com/contentgrid/appserver/infrastructure/impl/fs/zip/ZipArtifactEntry.java
+++ contentgrid-appserver-infrastructure-impl-fs/src/main/java/com/contentgrid/appserver/infrastructure/impl/fs/zip/ZipArtifactEntry.java
@@ -3,6 +3,7 @@ package com.contentgrid.appserver.infrastructure.impl.fs.zip;
 import com.contentgrid.appserver.infrastructure.api.ArtifactEntry;
 import com.contentgrid.appserver.infrastructure.api.ArtifactEntryReference;
 import com.contentgrid.appserver.infrastructure.api.ArtifactEntryUnreadableException;
+import java.io.Closeable;
 import java.io.FilterInputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -22,20 +23,45 @@ public class ZipArtifactEntry implements ArtifactEntry {
     public InputStream getInputStream() throws ArtifactEntryUnreadableException {
         try {
             var zipFile = new ZipFile(zipPath.toFile());
-            var entry = zipFile.getEntry(entryReference.getRelativePath());
-            var stream = zipFile.getInputStream(entry);
-            return new FilterInputStream(stream) {
-                @Override
-                public void close() throws IOException {
-                    try {
-                        super.close();
-                    } finally {
-                        zipFile.close();
+            try (var holder = new DetachableCloseable(zipFile)) {
+                var entry = zipFile.getEntry(entryReference.getRelativePath());
+                var stream = zipFile.getInputStream(entry);
+                var result = new FilterInputStream(stream) {
+                    @Override
+                    public void close() throws IOException {
+                        try {
+                            super.close();
+                        } finally {
+                            zipFile.close();
+                        }
                     }
-                }
-            };
+                };
+                holder.detach();
+                return result;
+            }
         } catch (IOException e) {
             throw new ArtifactEntryUnreadableException(entryReference, e);
+        }
+    }
+
+    // Making sure that zipFile gets closed when something goes wrong before returning
+    private static class DetachableCloseable implements Closeable {
+        private final Closeable delegate;
+        private boolean detached;
+
+        DetachableCloseable(Closeable delegate) {
+            this.delegate = delegate;
+        }
+
+        void detach() {
+            this.detached = true;
+        }
+
+        @Override
+        public void close() throws IOException {
+            if (!detached) {
+                delegate.close();
+            }
         }
     }
 }
