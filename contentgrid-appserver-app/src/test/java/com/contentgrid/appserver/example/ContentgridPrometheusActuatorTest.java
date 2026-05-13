ad9e1471734f3e9397a30b5f5f93f0511831fe68
Robrecht Vanhuysse <robrecht.vanhuysse@xenit.eu>
2026-05-13 13:09:08 +0200

    ACC-2941 Re-enable prometheus actuator endpoint
    This actuator bean is conditional on micrometer-registry classes being on the classpath.
    By including the added dependency as `runtimeOnly` we ensure this is the case, triggering the creation of the actuator bean.

---
+++ contentgrid-appserver-app/src/test/java/com/contentgrid/appserver/example/ContentgridPrometheusActuatorTest.java
@@ -0,0 +1,48 @@
+package com.contentgrid.appserver.example;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.context.ApplicationContext;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest(
+        webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
+        properties = {
+                "management.endpoints.web.exposure.include=*",
+                "management.prometheus.metrics.export.enabled=true", //bootRun profile disables metrics, which is fine for other tests but should be overridden here
+                "management.server.port=0" // random, different port from main port
+        }
+)
+@ActiveProfiles("bootRun")
+@Slf4j
+public class ContentgridPrometheusActuatorTest {
+    @Autowired
+    private TestRestTemplate rest;
+
+    @Value("${local.management.port}")
+    int managementPort;
+
+    @SpringBootApplication
+    static class TestApplication {
+        public static void main(String[] args) {
+            SpringApplication.run(TestApplication.class, args);
+        }
+    }
+
+    @Test
+    void prometheusEndpointIsPublic() {
+        ResponseEntity<String> resp = rest.getForEntity("http://localhost:" + managementPort + "/actuator/prometheus", String.class);
+        log.debug(resp.toString());
+        assertThat(resp.getStatusCode().is2xxSuccessful()).isTrue();
+    }
+}
