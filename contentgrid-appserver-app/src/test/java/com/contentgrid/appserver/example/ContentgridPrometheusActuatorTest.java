8a7ecced540a0955e7cab9baf7cdc81263880c9b
Robrecht Vanhuysse <robrecht.vanhuysse@xenit.eu>
2026-05-18 10:37:18 +0200

    ACC-2941 resolve sonarqube issues

--- contentgrid-appserver-app/src/test/java/com/contentgrid/appserver/example/ContentgridPrometheusActuatorTest.java
+++ contentgrid-appserver-app/src/test/java/com/contentgrid/appserver/example/ContentgridPrometheusActuatorTest.java
@@ -1,17 +1,12 @@
 package com.contentgrid.appserver.example;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatCode;
 
-import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.context.ApplicationContext;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
 
@@ -24,7 +19,7 @@ import org.springframework.test.context.ActiveProfiles;
         }
 )
 @ActiveProfiles("bootRun")
-public class ContentgridPrometheusActuatorTest {
+class ContentgridPrometheusActuatorTest {
     @Autowired
     private TestRestTemplate rest;
 
