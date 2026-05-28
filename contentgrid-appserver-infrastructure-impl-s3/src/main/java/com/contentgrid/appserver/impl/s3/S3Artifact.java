2ecce8479ddd4a6ed65fa8ddff1ccff0478a46fe
niels cappelle <niels.cappelle@xenit.eu>
2026-05-28 11:49:38 +0200

    ACC-2780 let S3Artifact extend AbstractRemoteArtifact

--- contentgrid-appserver-infrastructure-impl-s3/src/main/java/com/contentgrid/appserver/impl/s3/S3Artifact.java
+++ contentgrid-appserver-infrastructure-impl-s3/src/main/java/com/contentgrid/appserver/impl/s3/S3Artifact.java
@@ -1,7 +1,7 @@
 package com.contentgrid.appserver.impl.s3;
 
+import com.contentgrid.appserver.infrastructure.api.AbstractRemoteArtifact;
 import com.contentgrid.appserver.infrastructure.api.Artifact;
-import com.contentgrid.appserver.infrastructure.api.ArtifactEntry;
 import com.contentgrid.appserver.infrastructure.api.ArtifactException;
 import com.contentgrid.appserver.infrastructure.api.ArtifactReference;
 import com.contentgrid.appserver.infrastructure.impl.fs.zip.ZipArtifact;
@@ -10,18 +10,16 @@ import io.minio.MinioAsyncClient;
 import io.minio.errors.MinioException;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
+import java.nio.file.attribute.PosixFilePermissions;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
-import java.util.List;
-import java.util.Optional;
 import java.util.concurrent.ExecutionException;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
-public class S3Artifact implements Artifact {
+public class S3Artifact extends AbstractRemoteArtifact {
 
     public static final String SCHEME = "s3";
 
@@ -34,38 +32,17 @@ public class S3Artifact implements Artifact {
     @NonNull
     private final String objectKey;
 
-    private volatile ZipArtifact zipArtifact;
-
     @Override
     public ArtifactReference getReference() {
         return ArtifactReference.of(SCHEME + ":" + bucketName + "/" + objectKey);
     }
 
     @Override
-    public Optional<ArtifactEntry> load(Path path) throws ArtifactException {
-        return delegate().load(path);
-    }
-
-    @Override
-    public List<ArtifactEntry> loadAll(Path path) throws ArtifactException {
-        return delegate().loadAll(path);
-    }
-
-    private ZipArtifact delegate() throws ArtifactException {
-        if (zipArtifact == null) {
-            synchronized (this) {
-                if (zipArtifact == null) {
-                    zipArtifact = download();
-                }
-            }
-        }
-        return zipArtifact;
-    }
-
-    private ZipArtifact download() throws ArtifactException {
+    protected Artifact createDelegate() throws ArtifactException {
         var ref = getReference();
         try {
-            var tmpFile = Files.createTempFile("s3artifact-", ".zip");
+            var tmpFile = Files.createTempFile("s3artifact-", ".zip",
+                    PosixFilePermissions.asFileAttribute(PosixFilePermissions.fromString("rw-------")));
             tmpFile.toFile().deleteOnExit();
 
             try (var response = client.getObject(GetObjectArgs.builder()
