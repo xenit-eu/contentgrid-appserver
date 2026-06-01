908847966ffb06c61bca468d86aeabf805479558
niels cappelle <niels.cappelle@xenit.eu>
2026-06-01 15:45:43 +0200

    ACC-2745 remove OpenApiController for openapi.yml
    openapi.yml is no longer generated with scribe

--- contentgrid-appserver-webjars/src/main/java/com/contentgrid/appserver/webjars/WebjarsRestConfiguration.java
+++ contentgrid-appserver-webjars/src/main/java/com/contentgrid/appserver/webjars/WebjarsRestConfiguration.java
@@ -1,22 +1,12 @@
 package com.contentgrid.appserver.webjars;
 
-import com.contentgrid.appserver.infrastructure.api.Artifact;
 import com.contentgrid.appserver.webjars.hal.explorer.HalExplorerController;
-import com.contentgrid.appserver.webjars.swagger.ui.OpenApiController;
 import com.contentgrid.appserver.webjars.swagger.ui.SwaggerUIInitializerController;
-import lombok.SneakyThrows;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 @Configuration
 @Import({SwaggerUIInitializerController.class, HalExplorerController.class})
 public class WebjarsRestConfiguration {
-
-    @Bean
-    @SneakyThrows
-    OpenApiController openApiController(Artifact artifact) {
-        return new OpenApiController(artifact);
-    }
 
 }
