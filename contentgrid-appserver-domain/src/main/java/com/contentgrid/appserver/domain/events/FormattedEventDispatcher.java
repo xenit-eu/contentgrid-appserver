e0f74b4196bf7d729b8b5975fe12c7836f7f3485
Roel <roel.schevenels@xenit.eu>
2026-06-15 11:47:20 +0200

    Remove unused entity formatter code
    This was unused code (except for that bit in the test) and its purpose
    is in reality handled by DomainEventDispatcher.

--- contentgrid-appserver-domain/src/main/java/com/contentgrid/appserver/domain/events/FormattedEventDispatcher.java
+++
@@ -1,9 +0,0 @@
-package com.contentgrid.appserver.domain.events;
-
-import com.fasterxml.jackson.core.TreeNode;
-
-public interface FormattedEventDispatcher {
-    void dispatchCreate(String entity, TreeNode newData);
-    void dispatchUpdate(String entity, TreeNode oldData, TreeNode newData);
-    void dispatchDelete(String entity, TreeNode oldData);
-}
--- contentgrid-appserver-rest/src/test/java/com/contentgrid/appserver/rest/mapping/DynamicDispatchApplicationHandlerMappingTest.java
+++ contentgrid-appserver-rest/src/test/java/com/contentgrid/appserver/rest/mapping/DynamicDispatchApplicationHandlerMappingTest.java
@@ -29,15 +29,9 @@ import com.contentgrid.appserver.application.model.values.TableName;
 import com.contentgrid.appserver.autoconfigure.domain.ContentGridDomainAutoConfiguration;
 import com.contentgrid.appserver.autoconfigure.rest.ContentGridRestAutoConfiguration;
 import com.contentgrid.appserver.autoconfigure.rest.ContentGridRestFormatterAutoConfiguration;
-import com.contentgrid.appserver.domain.data.EntityInstance;
-import com.contentgrid.appserver.domain.events.EntityFormatter;
 import com.contentgrid.appserver.registry.ApplicationNameExtractor;
 import com.contentgrid.appserver.registry.ApplicationResolver;
 import com.contentgrid.appserver.rest.mapping.SpecializedOnPropertyType.PropertyType;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.MapperFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.json.JsonMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -147,19 +141,6 @@ class DynamicDispatchApplicationHandlerMappingTest {
                 @Override
                 public void addCorsMappings(CorsRegistry registry) {
                     registry.addMapping("/**").allowedOrigins("http://localhost:1234").allowedMethods("*");
-                }
-            };
-        }
-
-        @Bean
-        public EntityFormatter dummyFormatter() {
-            return new EntityFormatter() {
-                private final ObjectMapper mapper = JsonMapper.builder()
-                        .configure(MapperFeature.REQUIRE_HANDLERS_FOR_JAVA8_TIMES, false)
-                        .build();
-                @Override
-                public JsonNode format(Application application, EntityInstance entityInstance) {
-                    return mapper.valueToTree(entityInstance);
                 }
             };
         }
