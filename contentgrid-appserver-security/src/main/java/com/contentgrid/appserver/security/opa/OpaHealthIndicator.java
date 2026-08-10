1ba4b2796b82baa5c0019e86d23fa1d238207f5b
andreasvranken <andreas.vranken@amexiogroup.com>
2026-08-10 16:58:58 +0200

    [ACC-3047] Remove unused annotation

--- contentgrid-appserver-security/src/main/java/com/contentgrid/appserver/security/opa/OpaHealthIndicator.java
+++ contentgrid-appserver-security/src/main/java/com/contentgrid/appserver/security/opa/OpaHealthIndicator.java
@@ -3,9 +3,7 @@ package com.contentgrid.appserver.security.opa;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.health.contributor.Health;
 import org.springframework.boot.health.contributor.HealthIndicator;
-import org.springframework.stereotype.Component;
 
-@Component
 @RequiredArgsConstructor
 public class OpaHealthIndicator implements HealthIndicator {
 
