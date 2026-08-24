fe9eb1dfee7103776aae3798d8f2b40de2ed13e4
andreasvranken <andreas.vranken@amexiogroup.com>
2026-08-24 12:06:15 +0200

    [ACC-3047] Add equals and hashCode and toString methods to satisfy sonar

--- contentgrid-appserver-actuators/src/main/java/com/contentgrid/appserver/actuator/policy/PolicyBundle.java
+++ contentgrid-appserver-actuators/src/main/java/com/contentgrid/appserver/actuator/policy/PolicyBundle.java
@@ -1,5 +1,8 @@
 package com.contentgrid.appserver.actuator.policy;
 
+import java.util.Objects;
+import lombok.NonNull;
+
 /**
  * An OPA bundle together with the entity tag that identifies its contents.
  *
@@ -8,4 +11,24 @@ package com.contentgrid.appserver.actuator.policy;
  */
 public record PolicyBundle(byte[] content, String etag) {
 
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof PolicyBundle that)) {
+            return false;
+        }
+        return Objects.equals(etag(), that.etag());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(etag());
+    }
+
+    @Override
+    @NonNull
+    public String toString() {
+        return "PolicyBundle{" +
+                "etag='" + etag + '\'' +
+                '}';
+    }
 }
